cbab76b870af09ca147f685aeada6a682ee0d5e7
rektide <rektide+scm@voodoowarez.com>
2026-02-08 03:03:30 -0500

    init: import modules to trigger self-registration

--- lua/termichatter/init.lua
+++ lua/termichatter/init.lua
@@ -5,6 +5,11 @@ local M = {}
 local pipeline = require("termichatter.pipeline")
 local registry = require("termichatter.registry")
 
+-- Import modules so they register themselves
+require("termichatter.processors")
+require("termichatter.consumer")
+require("termichatter.outputters")
+
 -- Start with pipeline as base (log methods inherited via __index)
 setmetatable(M, { __index = pipeline })
 
